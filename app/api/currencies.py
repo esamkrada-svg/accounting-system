ad5fb8ce26a846cfad9f3e5d4b6442d8483efab8
esamkrada-svg <esam.krada@gmail.com>
2026-02-02 06:24:11 +0300

    Update currencies.py

--- app/api/currencies.py
+++ app/api/currencies.py
@@ -1,1 +1,22 @@
+from fastapi import APIRouter, Depends, Header
+from sqlalchemy.orm import Session
 
+from app.database.database import SessionLocal
+from app.database.models import Currency
+from app.api.auth import get_current_user
+
+router = APIRouter(prefix="/api/currencies", tags=["API Currencies"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/")
+def list_currencies(token: str = Header(...), db: Session = Depends(get_db)):
+    get_current_user(token)
+    return db.query(Currency).all()
