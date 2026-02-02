29acfbf8e814faa9483639045c68b8547db63073
esamkrada-svg <esam.krada@gmail.com>
2026-02-02 04:56:46 +0300

    Add account service functions for CRUD operations

--- app/modules/accounts/service.py
+++ app/modules/accounts/service.py
@@ -1,1 +1,18 @@
+from sqlalchemy.orm import Session
+from app.database.models import Account
 
+
+def get_all_accounts(db: Session):
+    return db.query(Account).order_by(Account.code).all()
+
+
+def create_account(db: Session, code: str, name: str, type: str):
+    account = Account(
+        code=code,
+        name=name,
+        type=type
+    )
+    db.add(account)
+    db.commit()
+    db.refresh(account)
+    return account
