618026667f1bc5eea65d331d275989704892f694
esamkrada-svg <esam.krada@gmail.com>
2026-02-02 05:49:40 +0300

    Update schemas.py

--- app/modules/currencies/schemas.py
+++ app/modules/currencies/schemas.py
@@ -1,1 +1,16 @@
+from pydantic import BaseModel
 
+
+class CurrencyCreate(BaseModel):
+    code: str
+    name: str
+
+
+class CurrencyOut(BaseModel):
+    id: int
+    code: str
+    name: str
+    active: bool
+
+    class Config:
+        orm_mode = True
