039933a51dc1651b2673cf8ee2a9833d7f00014b
esamkrada-svg <esam.krada@gmail.com>
2026-02-02 05:46:47 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,27 +3,25 @@ from app.database.database import init_db
 from app.modules.accounts.routes import router as accounts_router
 from app.modules.persons.routes import router as persons_router
 from app.modules.journal.routes import router as journal_router
+from app.modules.reports.routes import router as reports_router
 
 app = FastAPI(
     title="Accounting System",
-    description="Modular Accounting System (Arabic / English)",
     version="1.0.0"
 )
 
 
 @app.on_event("startup")
-def on_startup():
+def startup():
     init_db()
 
 
 app.include_router(accounts_router)
 app.include_router(persons_router)
 app.include_router(journal_router)
+app.include_router(reports_router)
 
 
 @app.get("/")
 def root():
-    return {
-        "status": "ok",
-        "message": "Accounting system is running"
-    }
+    return {"status": "ok"}
